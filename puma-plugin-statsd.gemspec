991baf7f84fb5a596f2bdb95df035317f2c9a17a
D-Rock <derek.gallo@gmail.com>
2019-10-18 15:23:49 -0400

    Config tests

--- puma-plugin-statsd.gemspec
+++ puma-plugin-statsd.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "minitest", "~> 5.0"
   spec.add_development_dependency "sinatra"
   spec.add_development_dependency "rack"
+  spec.add_development_dependency "everfi-gemfury"
 end
--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -4,4 +4,25 @@ class PluginTest < Minitest::Test
   def test_registration
     assert_kind_of Class, Puma::Plugins.find("statsd")
   end
+
+  def test_tags_empty_default
+    assert_empty plugin.send(:tags)
+  end
+
+  def test_tags_from_env
+    ENV['MY_POD_NAME'] = 'sample_pod'
+    ENV['STATSD_GROUPING'] = 'sample_grouping'
+    tags = plugin.send(:tags)
+
+    assert_equal 'sample_pod', tags[:pod_name]
+    assert_equal 'sample_grouping', tags[:grouping]
+  end
+
+  def teardown
+    %w[STATSD_HOST STATSD_PORT MY_POD_NAME STATSD_GROUPING].each {|var| ENV.delete var }
+  end
+
+  def plugin
+    Puma::PluginLoader.new.create("statsd")
+  end
 end
