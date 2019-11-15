663d4920f1e71a8338dc0f689332b268c88c3553
D-Rock <derek.gallo@gmail.com>
2019-11-15 15:59:29 -0500

    adds test for PumaStats class

---
+++ test/puma_stats_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class PumaStatsTest < MiniTest::Test
+  extend Minitest::Spec::DSL
+
+  let(:cluster_statistics) do
+    {
+        "workers" => 2,
+        "booted_workers" => 2,
+        "worker_status" => [worker_statistics,worker_statistics].map {|w| {"last_status" => w}}
+    }
+  end
+
+  let(:worker_statistics) do
+    {
+        "running" => 1,
+        "backlog" => 5,
+        "pool_capacity" => 2,
+        "max_threads" => 3
+    }
+  end
+
+  let(:cluster_stats) { PumaStats.new(cluster_statistics) }
+  let(:worker_stats) { PumaStats.new(worker_statistics) }
+
+  def setup
+    # Do nothing
+  end
+
+  def test_clustered?
+    assert cluster_stats.clustered?
+    refute worker_stats.clustered?
+  end
+
+  def test_workers
+    assert_equal 2,cluster_stats.workers
+    assert_equal 1,worker_stats.workers
+  end
+
+  def test_booted_workers
+    assert_equal 2,cluster_stats.booted_workers
+    assert_equal 1,worker_stats.booted_workers
+  end
+
+  def test_running
+    assert_equal 2, cluster_stats.running
+    assert_equal 1, worker_stats.running
+  end
+
+  def test_backlog
+    assert_equal 10, cluster_stats.backlog
+    assert_equal 5, worker_stats.backlog
+  end
+
+  def test_pool_capacity
+    assert_equal 4, cluster_stats.pool_capacity
+    assert_equal 2, worker_stats.pool_capacity
+  end
+
+  def test_max_threads
+    assert_equal 6, cluster_stats.max_threads
+    assert_equal 3, worker_stats.max_threads
+  end
+
+end
