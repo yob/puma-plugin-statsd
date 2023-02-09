9dd3d486b1ebf785906a51980dee8e849750880f
Victor Montes <vmontes@everfi.com>
2023-02-09 13:18:32 -0800

    FC-1729 Update tags specs to avoid false positives

--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -12,19 +12,19 @@ class PluginTest < Minitest::Test
   def test_tags_from_env
     ENV['MY_POD_NAME'] = 'sample_pod'
     ENV['STATSD_GROUPING'] = 'sample_grouping'
-    tags = plugin.send(:environment_variable_tags)
+    tags = plugin.send(:environment_variable_tags).split(',')
 
     assert_includes tags, 'pod_name:sample_pod'
     assert_includes tags, 'grouping:sample_grouping'
   end
 
   def test_tags_from_config
-    PumaStatsd.config.pod_name = 'sample_pod'
-    PumaStatsd.config.statsd_grouping = 'sample_grouping'
-    tags = plugin.send(:environment_variable_tags)
+    PumaStatsd.config.pod_name = 'pod_name_from_config'
+    PumaStatsd.config.statsd_grouping = 'grouping_from_config'
+    tags = plugin.send(:environment_variable_tags).split(',')
 
-    assert_includes tags, 'pod_name:sample_pod'
-    assert_includes tags, 'grouping:sample_grouping'
+    assert_includes tags, 'pod_name:pod_name_from_config'
+    assert_includes tags, 'grouping:grouping_from_config'
   end
 
   def teardown
